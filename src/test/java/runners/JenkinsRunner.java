54e39d89f543e12ee8caccd3c255b8098b485d38
Temirlan Seiitbekov <temirlan.seiitbekov@noveogroup.com>
2022-08-06 07:46:04 +0600

    test

---
+++ src/test/java/runners/JenkinsRunner.java
@@ -0,0 +1,16 @@
+package runners;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = {"classpath:features"}, // feature file folder name
+        glue = {"stepDefs"}, // folder with step definitions
+        plugin = {""},
+        tags = "@jenkins"
+)
+public class JenkinsRunner {
+}
